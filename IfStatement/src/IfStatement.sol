bf6d4cd38fd69ce04a86d2c04544b7e97f25cc90
Timmy Jose <zoltan.jose@gmail.com>
2023-10-31 21:10:56 +0530

    If statements.

--- IfStatement/src/IfStatement.sol
+++ IfStatement/src/IfStatement.sol
@@ -2,11 +2,20 @@
 pragma solidity ^0.8.13;
 
 contract IfStatement {
-    function max(uint256 a, uint256 b) public pure returns (uint256) {
-        // return the maximum of a and b
+  function max(uint256 a, uint256 b) public pure returns (uint256) {
+    // return the maximum of a and b
+    if (a > b) {
+      return a;
     }
+    return b;
+  }
 
-    function min(uint256 a, uint256 b) public pure returns (uint256) {
-        // return the minimum of a and b
+  function min(uint256 a, uint256 b) public pure returns (uint256) {
+    // return the minimum of a and b
+    if (a < b) {
+      return a;
+    } else {
+      return b;
     }
+  }
 }

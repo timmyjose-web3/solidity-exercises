54c1142964df44003e9d21f180f6ff6315490d28
Timmy Jose <zoltan.jose@gmail.com>
2023-11-01 10:22:54 +0530

    IsPrime, Fibonacci.

--- Fibonacci/src/Fibonacci.sol
+++ Fibonacci/src/Fibonacci.sol
@@ -12,6 +12,13 @@ contract Fibonacci {
     */
 
     function fibonacci(uint256 _position) public view returns (uint256) {
-        // your code here
+      // your code here
+      if (_position == 0) {
+        return 0;
+      } else if (_position == 1) {
+        return 1;
+      } else {
+        return fibonacci(_position - 1) + fibonacci(_position - 2); 
+      }
     }
 }
--- IsPrime/src/IsPrime.sol
+++ IsPrime/src/IsPrime.sol
@@ -5,7 +5,18 @@ contract IsPrime {
     /**
      * The goal of this exercise is to return if "number" is prime or not (true or false)
      */
-    function isPrime(uint256 number) public view returns (bool) {
+    function isPrime(uint256 number) public pure returns (bool) {
         // your code here
+      if (number <= 1) {
+        return false;
+      }
+
+      for (uint256 f = 2; f < number; f++) {
+        if (number % f == 0) {
+          return false;
+        }
+      }
+
+      return true;
     }
 }
